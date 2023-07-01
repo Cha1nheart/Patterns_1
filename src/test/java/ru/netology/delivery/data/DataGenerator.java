903fa0f38d356972d0b585543f6f46cd097c2ad2
Cha1nHeart <humanoverrated@gmail.com>
2023-07-01 11:28:36 +0500

    comments deleted

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -13,35 +13,25 @@ public class DataGenerator {
     private DataGenerator() {
     }
 
-    // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
-    // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
     public static String generateDate(int shift) {
         return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
-    // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
-    // с помощью Faker, либо используя массив валидных городов и класс Random
     public static String generateCity() {
         var cities = new String[]{"Майкоп", "Горно-Алтайск", "Уфа"};
         return cities[new Random().nextInt(cities.length)];
     }
 
-    // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
-    // использовать Faker
     public static String generateName(String locale) {
         var faker = new Faker(new Locale(locale));
         return faker.name().lastName() + " " + faker.name().firstName();
     }
 
-    // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
-    // использовать Faker
     public static String generatePhone(String locale) {
         var faker = new Faker(new Locale(locale));
         return faker.phoneNumber().phoneNumber();
     }
 
-    // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
-    // generateName(locale), generatePhone(locale)
     public static class Registration {
         private Registration() {
         }
